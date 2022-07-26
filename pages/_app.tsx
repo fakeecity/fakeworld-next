c19217d97c0f7ad1bc0491ee16b45cf87edc9413
Fake <34991992+fakeecity@users.noreply.github.com>
2022-07-26 19:04:54 -0400

    Update Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AnimatePresence } from "framer-motion";
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   const url = `https://fake.fm${router.route}`
 
   return(
+    <>
+    <Head>
+      <title>FAKEWORLD</title>
+      <link rel="icon" href="/icon.ico" />
+    </Head>
     <AnimatePresence exitBeforeEnter>
       <Component {...pageProps} canonical={url} key={url} />
     </AnimatePresence>
+    </>
   )
 }
 
